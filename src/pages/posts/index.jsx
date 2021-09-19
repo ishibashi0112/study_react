b4c2f62c658f495a1c5cfe70f27266856333ab21
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2021-09-19 11:31:11 +0900

    🚚 投稿系ページの名前変更

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,18 +1,18 @@
 import Head from 'next/head';
 import styles from 'src/styles/Home.module.css';
 import { Header } from 'src/components/Header';
-import {Posts} from "src/components/posts"
+import {Posts as PostCompornent} from "src/components/posts"
 
-const Home = () =>{
+const  Posts = () =>{
   return (
     <div className={styles.container}>
       <Head>
         <title>index page</title>
       </Head>
       <Header />
-      <Posts /> 
+      <PostCompornent /> 
     </div>
   )
 }
 
-export default Home;
+export default Posts;
