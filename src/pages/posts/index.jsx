951f00c0e58000ccf1cdbb7f2e4577714bd681ed
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2021-09-19 15:27:21 +0900

    🎨 Layoutwoをコンポーネントを使ってまとめた

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,16 +1,15 @@
 import Head from 'next/head';
-import styles from 'src/styles/Home.module.css';
 import { Header } from 'src/components/Header';
-import {Posts as PostCompornent} from "src/components/posts"
+import {Posts as PostComponent} from "src/components/posts"
 
 const  Posts = () =>{
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>index page</title>
       </Head>
       <Header />
-      <PostCompornent /> 
+      <PostComponent /> 
     </div>
   )
 }
